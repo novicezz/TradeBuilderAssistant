85faf31cc9871f01b0460507402b60d5e76b4ea1
Mark <markmao561@gmail.com>
2024-01-22 02:05:53 -0600

    Added short position mechanism, program is now in MVP stage; fixed logic error with activation rule calculation for long_position

--- app/modules/mechanisms.py
+++ app/modules/mechanisms.py
@@ -13,9 +13,9 @@ FIFTEEN_SLBUFFER = 0.02
 FIFTEENORHOUR_ENTRYBUFFER = 0.02
 DAY_ENTRYBUFFER = 0.04
 
+# Tradebuilder mechanisms
 def long_position(setupTable: dict) -> str:
 # Convert to Common Calculation
-    setupTable["entry"] = setupTable["dzone"]["proximal"]
     dzSize = setupTable["dzone"]["proximal"] - setupTable["dzone"]["distal"]
 # Error conditions
     if setupTable["dzone"]["proximal"] <= setupTable["dzone"]["distal"]:
@@ -25,13 +25,19 @@ def long_position(setupTable: dict) -> str:
     elif setupTable["szone"]["proximal"] <= setupTable["dzone"]["proximal"]:
         return "Error: Value of SZ proximal cannot be equal to or lower than value of DZ proximal"
 # Activation rule calculation
+    activationRule = None
     if dzSize <= 0.5:
         activationPrice = setupTable["entry"] - (dzSize / 5)
     elif 0.5 < dzSize <= 1.0:
         activationPrice = setupTable["entry"] - (dzSize / 4)
-    elif dzSize <= 1.0:
+    elif dzSize > 1.0:
         activationPrice = setupTable["entry"] - (dzSize / 3)
-    activationPrice = rounddown(activationPrice, HUNDREDTH)
+    else:
+        return "Error with activation rule input, please double check and try again"
+    if activationPrice is not None:
+        activationRule = rounddown(activationPrice, HUNDREDTH)
+    else:
+        return "Error: Activation rule cannot be generated with given SZ values, please double check and try again"
 # Limit/stop price calculation   
     if setupTable["timeframe"].get_val() == "15" or setupTable["timeframe"].get_val() == "hour":
         limitBuffer = FIFTEENORHOUR_ENTRYBUFFER
@@ -48,7 +54,7 @@ def long_position(setupTable: dict) -> str:
 # Position size calculator
     riskPerShare = abs(setupTable["entry"] - stopLoss)
     positionSize = floor(setupTable["maxrisk"] / riskPerShare)
-    positionCost = positionSize * setupTable["entry"]
+    positionCost = roundreg(positionSize * setupTable["entry"], HUNDREDTH)
     maxLiquidity = rounddown(setupTable["accsize"] * LIQUIDITY_FRACTION, HUNDREDTH)
     if positionCost > maxLiquidity:
         positionCost = maxLiquidity
@@ -67,7 +73,7 @@ def long_position(setupTable: dict) -> str:
     report += f"Position risk:      {tradeRisk}\n"
     report += f"Limit price:        {limitPrice}\n"
     report += f"Stop price:         {stopPrice}\n"
-    report += f"Activation price:   {activationPrice}\n"
+    report += f"Activation price:   {activationRule}\n"
     report += f"Target 1:           {targetOne}\n"
     report += f"Target 2:           {targetTwo}\n"
     report += f"Target 1 split:     {targetOneShares}\n"
@@ -75,10 +81,70 @@ def long_position(setupTable: dict) -> str:
     return report
 
 def short_position(setupTable: dict):
-    setupTable["entry"] = setupTable["szone"]["proximal"]
+    # Convert to Common Calculation
     szSize = setupTable["szone"]["distal"] - setupTable["szone"]["proximal"]
-    pass
-    # stopLoss = setupTable["szone"]["distal"] + (setupTable["atr"] * stopLossBuffer) for Short
+# Error conditions
+    if setupTable["dzone"]["proximal"] <= setupTable["dzone"]["distal"]:
+        return "Error: Value of DZ proximal cannot be equal to or lower than value of DZ distal"
+    elif setupTable["szone"]["distal"] <= setupTable["szone"]["proximal"]:
+        return "Error: Value of SZ distal cannot be equal to or lower than value of SZ proximal"
+    elif setupTable["szone"]["proximal"] <= setupTable["dzone"]["proximal"]:
+        return "Error: Value of SZ proximal cannot be equal to or lower than value of DZ proximal"
+# Activation rule calculation
+    activationRule = None
+    if szSize <= 0.5:
+        activationPrice = setupTable["entry"] + (szSize / 5)
+    elif 0.5 < szSize <= 1.0:
+        activationPrice = setupTable["entry"] + (szSize / 4)
+    elif szSize > 1.0:
+        activationPrice = setupTable["entry"] + (szSize / 3)
+    else:
+        return "Error with activation rule input, please double check and try again"
+    if activationPrice is not None:
+        activationRule = roundup(activationPrice, HUNDREDTH)
+    else:
+        return "Error: Activation rule cannot be generated with given SZ values, please double check and try again"
+# Limit/stop price calculation
+    if setupTable["timeframe"].get_val() == "15" or setupTable["timeframe"].get_val() == "hour":
+        limitBuffer = FIFTEENORHOUR_ENTRYBUFFER
+    elif setupTable["timeframe"].get_val() == "day":
+        limitBuffer = DAY_ENTRYBUFFER
+    limitPrice = roundreg(setupTable["entry"] - limitBuffer, HUNDREDTH)
+    stopPrice = setupTable["entry"]
+# Stop loss calculation
+    if setupTable["timeframe"].get_val() == "hour" or setupTable["timeframe"].get_val() == "day": 
+        stopLossBuffer = DAYORHOUR_SLBUFFER
+    elif setupTable["timeframe"].get_val() == "15":
+        stopLossBuffer = FIFTEEN_SLBUFFER
+    stopLoss = setupTable["szone"]["distal"] + (setupTable["atr"] * stopLossBuffer)
+# Position size calculator
+    riskPerShare = abs(stopLoss - setupTable["entry"])
+    positionSize = floor(setupTable["maxrisk"] / riskPerShare)
+    positionCost = roundreg(positionSize * setupTable["entry"], HUNDREDTH)
+    maxLiquidity = rounddown(setupTable["accsize"] * LIQUIDITY_FRACTION, HUNDREDTH)
+    if positionCost > maxLiquidity:
+        positionCost = maxLiquidity
+        positionSize = floor(positionCost / setupTable["entry"])
+        print("Max account liquidity insufficient, position size reduced to:", positionSize)
+    tradeRisk = roundreg(positionSize * riskPerShare, HUNDREDTH)
+# Target Calculator
+    targetOne = setupTable["entry"] - roundup(0.8 * abs(setupTable["entry"] - setupTable["dzone"]["proximal"]), HUNDREDTH)
+    targetOneShares = ceil(0.8 * positionSize)
+    targetTwo = setupTable["dzone"]["proximal"]
+    targetTwoShares = positionSize - targetOneShares
+# Print position 
+    report  = f"Ticker:             {setupTable['ticker']}\n"
+    report += f"Position cost:      {positionCost}\n"
+    report += f"Position size:      {positionSize}\n"
+    report += f"Position risk:      {tradeRisk}\n"
+    report += f"Limit price:        {limitPrice}\n"
+    report += f"Stop price:         {stopPrice}\n"
+    report += f"Activation price:   {activationRule}\n"
+    report += f"Target 1:           {targetOne}\n"
+    report += f"Target 2:           {targetTwo}\n"
+    report += f"Target 1 split:     {targetOneShares}\n"
+    report += f"Target 2 split:     {targetTwoShares}\n"
+    return report
 
 def generate_position():
     setupTable = fill_table(MemoryManager.get_set())
