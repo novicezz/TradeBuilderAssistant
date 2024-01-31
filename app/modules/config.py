4b0c626a9f5074bde94351018286cf359db2eb1c
Mark <markmao561@gmail.com>
2024-01-31 10:24:01 -0600

    Standardized configuration setting

--- app/modules/config.py
+++ app/modules/config.py
@@ -1,8 +1,32 @@
+class ZoneSpec:
+    def __init__(self, lowest: float, zoneSet: dict | None):
+        self.lowestFract = lowest
+        self.zoneSet = zoneSet
+        if(zoneSet == None): self.sizes = None
+        else: self.sizes = list(zoneSet.keys()).sort()
+
+    def get_activation(self, szSize: float, entry: float) -> float:
+        activation = ZoneSpec.calc_activation(entry, szSize, self.lowestFract)
+        if(self.sizes == None):
+            return activation
+        for i in self.sizes:
+            if szSize > i:
+                return activation
+            activation = ZoneSpec.calc_activation(entry, szSize, self.zoneSet[i])
+        return activation
+
+    @staticmethod
+    def calc_activation(entry: float, szSize: float, fract: float) -> float:
+        return entry + szSize / fract
+
 DEFAULT_CONFIG = {
-    "LIQUIDITYFRACTION": 0.50,
-    "15":   { "ENTRYBUFFER":  0.02, "SLBUFFER": 0.02 },
-    "HOUR": { "ENTRYBUFFER":  0.02, "SLBUFFER": 0.10 },
-    "DAT":  { "ENTRYBUFFER":  0.04, "SLBUFFER": 0.10 }
+    "LIQUIDITY_FRACTION": 0.50,
+    "TARGET_BUFFERS":  [ 0.80, 1.00 ],
+    "TARGET_SPLIT": [ 0.80, 0.20 ],
+    "ZONE_SPEC": ZoneSpec(5, { 0.5: 4, 1.0: 3 }),
+    "15":   { "ENTRY_BUFFER":  0.02, "SL_BUFFER": 0.02 },
+    "hour": { "ENTRY_BUFFER":  0.02, "SL_BUFFER": 0.10 },
+    "day":  { "ENTRY_BUFFER":  0.04, "SL_BUFFER": 0.10 }
 }
 
 
--- app/modules/mechanisms.py
+++ app/modules/mechanisms.py
@@ -1,31 +1,14 @@
 # External imports
-from math import ceil, floor
+from math                   import ceil, floor
 # Local imports
-from modules.calculations import *
-from modules.utils  import get_primitives, check_if_full
+from modules.calculations   import *
+from modules.utils          import get_primitives, check_if_full
+import modules.config       as conf
 
 # Constants
 HUNDREDTH = 2
-LIQUIDITY_FRACTION = 0.5
 
-FIFTEEN_SLBUFFER = 0.02
-HOUR_SLBUFFER = 0.1
-DAY_SLBUFFER = 0.1
-
-FIFTEEN_ENTRYBUFFER = 0.02
-HOUR_ENTRYBUFFER = 0.02
-DAY_ENTRYBUFFER = 0.04
-
-TARGETONE_BUFFER = 0.8
-
-ZONEDENOMIATOR_FIFTH = 5
-ZONEDENOMIATOR_QUARTER = 4
-ZONEDENOMIATOR_THIRD = 3
-
-ZONESIZE_FIFTYCENTS = 0.5
-ZONESIZE_DOLLAR = 1.0
-
-def long_position(setupTable: dict) -> str:
+def long_position(setupTable: dict, configSettings: dict) -> str:
 # Convert to Common Calculation
     dzSize = setupTable["dzone"]["proximal"] - setupTable["dzone"]["distal"]
 # Error conditions
@@ -36,66 +19,49 @@ def long_position(setupTable: dict) -> str:
     elif setupTable["szone"]["proximal"] <= setupTable["dzone"]["proximal"]:
         return "Error: Value of SZ proximal cannot be equal to or lower than value of DZ proximal"
 # Activation rule calculation
-    activationRule = None
-    if dzSize <= ZONESIZE_FIFTYCENTS:
-        activationPrice = setupTable["entry"] - (dzSize / ZONEDENOMIATOR_FIFTH)
-    elif ZONESIZE_FIFTYCENTS < dzSize <= ZONESIZE_DOLLAR:
-        activationPrice = setupTable["entry"] - (dzSize / ZONEDENOMIATOR_QUARTER)
-    elif dzSize > ZONESIZE_DOLLAR:
-        activationPrice = setupTable["entry"] - (dzSize / ZONEDENOMIATOR_THIRD)
-    else:
-        return "Error with activation rule input, please double check and try again"
-    if activationPrice is not None:
-        activationRule = rounddown(activationPrice, HUNDREDTH)
-    else:
-        return "Error: Activation rule cannot be generated with given SZ values, please double check and try again"
-# Limit/stop price calculation   
-    if setupTable["timeframe"] == "15":
-        limitBuffer = FIFTEEN_ENTRYBUFFER
-    elif setupTable["timeframe"]== "hour":
-        limitBuffer = HOUR_ENTRYBUFFER
-    elif setupTable["timeframe"] == "day":
-        limitBuffer = DAY_ENTRYBUFFER
-    limitPrice = roundreg(setupTable["entry"] + limitBuffer, HUNDREDTH)
+    activationPrice = configSettings["ZONE_SPEC"].get_activation(dzSize, setupTable["entry"])
+    activationRule = roundup(activationPrice, HUNDREDTH)
+# Limit/stop price calculation
+    limitBuffer = configSettings[setupTable["timeframe"]]["ENTRY_BUFFER"]
+    limitPrice = roundreg(setupTable["entry"] - limitBuffer, HUNDREDTH)
     stopPrice = setupTable["entry"]
 # Stop loss calculation
-    if setupTable["timeframe"] == "15":
-        stopLossBuffer = FIFTEEN_SLBUFFER
-    elif setupTable["timeframe"] == "hour": 
-        stopLossBuffer = HOUR_SLBUFFER
-    elif setupTable["timeframe"] == "day":
-        stopLossBuffer = DAY_SLBUFFER
-    stopLoss = setupTable["dzone"]["distal"] - (setupTable["atr"] * stopLossBuffer)
+    stopLossBuffer = configSettings[setupTable["timeframe"]]["SL_BUFFER"]
+    stopLoss = setupTable["dzone"]["distal"] - roundreg(setupTable["atr"] * stopLossBuffer, HUNDREDTH)
 # Position size calculator
     riskPerShare = abs(setupTable["entry"] - stopLoss)
     positionSize = floor(setupTable["maxrisk"] / riskPerShare)
     positionCost = roundreg(positionSize * setupTable["entry"], HUNDREDTH)
-    maxLiquidity = rounddown(setupTable["accsize"] * LIQUIDITY_FRACTION, HUNDREDTH)
+    maxLiquidity = rounddown(setupTable["accsize"] * configSettings["LIQUIDITY_FRACTION"], HUNDREDTH)
     if positionCost > maxLiquidity:
         positionCost = maxLiquidity
         positionSize = floor(positionCost / setupTable["entry"])
         print("Max account liquidity insufficient, position size reduced to:", positionSize)
     tradeRisk = roundreg(positionSize * riskPerShare, HUNDREDTH)
 # Target Calculator
-    targetOne = setupTable["entry"] + rounddown(0.8 * abs(setupTable["szone"]["proximal"] - setupTable["entry"]), HUNDREDTH)
-    targetOneShares = ceil(TARGETONE_BUFFER * positionSize)
-    targetTwo = setupTable["szone"]["proximal"]
-    targetTwoShares = positionSize - targetOneShares
+    targets = []
+    targetShares = []
+    for i in range(0, len(configSettings["TARGET_BUFFERS"])):
+        distance = abs(setupTable["szone"]["proximal"] - setupTable["entry"])
+        targets.append(roundreg(setupTable["entry"] + rounddown(configSettings["TARGET_BUFFERS"][i] * distance, HUNDREDTH), HUNDREDTH))
+        targetShares.append(round(configSettings["TARGET_SPLIT"][i] * positionSize))
 # Assemble report
     report  = f"Ticker:             {setupTable['ticker']}\n"
-    report += f"Position cost:      {positionCost}\n"
+    report += f"Position cost:      ${positionCost}\n"
     report += f"Position size:      {positionSize}\n"
-    report += f"Position risk:      {tradeRisk}\n"
-    report += f"Limit price:        {limitPrice}\n"
-    report += f"Stop price:         {stopPrice}\n"
-    report += f"Activation price:   {activationRule}\n"
-    report += f"Target 1:           {targetOne}\n"
-    report += f"Target 2:           {targetTwo}\n"
-    report += f"Target 1 split:     {targetOneShares}\n"
-    report += f"Target 2 split:     {targetTwoShares}"
+    report += f"Position risk:      ${tradeRisk}\n"
+    report += f"Limit price:        ${limitPrice}\n"
+    report += f"Stop price:         ${stopPrice}\n"
+    report += f"Activation price:   ${activationRule}\n"
+    report += f"Stop Loss:          ${stopLoss}\n"
+    for i in range(0, len(targets)):
+        report += f"Target {i + 1}: ${targets[i]}\n"
+    for i in range(0, len(targetShares)):
+        report += f"Target {i + 1} split: {targetShares[i]}"
+        if i != len(targetShares) - 1: report += '\n'
     return report
 
-def short_position(setupTable: dict) -> str:
+def short_position(setupTable: dict, configSettings: dict) -> str:
     # Convert to Common Calculation
     szSize = setupTable["szone"]["distal"] - setupTable["szone"]["proximal"]
 # Error conditions
@@ -106,63 +72,46 @@ def short_position(setupTable: dict) -> str:
     elif setupTable["szone"]["proximal"] <= setupTable["dzone"]["proximal"]:
         return "Error: Value of SZ proximal cannot be equal to or lower than value of DZ proximal"
 # Activation rule calculation
-    activationRule = None
-    if szSize <= ZONESIZE_FIFTYCENTS:
-        activationPrice = setupTable["entry"] + (szSize / ZONEDENOMIATOR_FIFTH)
-    elif ZONESIZE_FIFTYCENTS < szSize <= ZONESIZE_DOLLAR:
-        activationPrice = setupTable["entry"] + (szSize / ZONEDENOMIATOR_QUARTER)
-    elif szSize > ZONESIZE_DOLLAR:
-        activationPrice = setupTable["entry"] + (szSize / ZONEDENOMIATOR_THIRD)
-    else:
-        return "Error with activation rule input, please double check and try again"
-    if activationPrice is not None:
-        activationRule = roundup(activationPrice, HUNDREDTH)
-    else:
-        return "Error: Activation rule cannot be generated with given SZ values, please double check and try again"
+    activationPrice = configSettings["ZONE_SPEC"].get_activation(szSize, setupTable["entry"])
+    activationRule = roundup(activationPrice, HUNDREDTH)
 # Limit/stop price calculation
-    if setupTable["timeframe"] == "15":
-        limitBuffer = FIFTEEN_ENTRYBUFFER
-    elif setupTable["timeframe"] == "hour":
-        limitBuffer = HOUR_ENTRYBUFFER
-    elif setupTable["timeframe"] == "day":
-        limitBuffer = DAY_ENTRYBUFFER
+    limitBuffer = configSettings[setupTable["timeframe"]]["ENTRY_BUFFER"]
     limitPrice = roundreg(setupTable["entry"] - limitBuffer, HUNDREDTH)
     stopPrice = setupTable["entry"]
 # Stop loss calculation
-    if setupTable["timeframe"] == "15":
-        stopLossBuffer = FIFTEEN_SLBUFFER
-    elif setupTable["timeframe"] == "hour": 
-        stopLossBuffer = HOUR_SLBUFFER
-    elif setupTable["timeframe"] == "day":
-        stopLossBuffer = DAY_SLBUFFER
-    stopLoss = setupTable["szone"]["distal"] + (setupTable["atr"] * stopLossBuffer)
+    stopLossBuffer = configSettings[setupTable["timeframe"]]["SL_BUFFER"]
+    stopLoss = setupTable["szone"]["distal"] +  roundreg(setupTable["atr"] * stopLossBuffer, HUNDREDTH)
 # Position size calculator
     riskPerShare = abs(stopLoss - setupTable["entry"])
     positionSize = floor(setupTable["maxrisk"] / riskPerShare)
     positionCost = roundreg(positionSize * setupTable["entry"], HUNDREDTH)
-    maxLiquidity = rounddown(setupTable["accsize"] * LIQUIDITY_FRACTION, HUNDREDTH)
+    maxLiquidity = rounddown(setupTable["accsize"] * configSettings["LIQUIDITY_FRACTION"], HUNDREDTH)
     if positionCost > maxLiquidity:
         positionCost = maxLiquidity
         positionSize = floor(positionCost / setupTable["entry"])
         print("Max account liquidity insufficient, position size reduced to:", positionSize)
     tradeRisk = roundreg(positionSize * riskPerShare, HUNDREDTH)
 # Target Calculator
-    targetOne = setupTable["entry"] - roundup(0.8 * abs(setupTable["entry"] - setupTable["dzone"]["proximal"]), HUNDREDTH)
-    targetOneShares = ceil(TARGETONE_BUFFER * positionSize)
-    targetTwo = setupTable["dzone"]["proximal"]
-    targetTwoShares = positionSize - targetOneShares
+    targets = []
+    targetShares = []
+    for i in range(0, len(configSettings["TARGET_BUFFERS"])):
+        distance = abs(setupTable["entry"] - setupTable["dzone"]["proximal"])
+        targets.append(roundreg(setupTable["entry"] - roundup(configSettings["TARGET_BUFFERS"][i] * distance, HUNDREDTH), HUNDREDTH))
+        targetShares.append(round(configSettings["TARGET_SPLIT"][i] * positionSize))
 # Print position 
     report  = f"Ticker:             {setupTable['ticker']}\n"
-    report += f"Position cost:      {positionCost}\n"
+    report += f"Position cost:      ${positionCost}\n"
     report += f"Position size:      {positionSize}\n"
-    report += f"Position risk:      {tradeRisk}\n"
-    report += f"Limit price:        {limitPrice}\n"
-    report += f"Stop price:         {stopPrice}\n"
-    report += f"Activation price:   {activationRule}\n"
-    report += f"Target 1:           {targetOne}\n"
-    report += f"Target 2:           {targetTwo}\n"
-    report += f"Target 1 split:     {targetOneShares}\n"
-    report += f"Target 2 split:     {targetTwoShares}"
+    report += f"Position risk:      ${tradeRisk}\n"
+    report += f"Limit price:        ${limitPrice}\n"
+    report += f"Stop price:         ${stopPrice}\n"
+    report += f"Activation price:   ${activationRule}\n"
+    report += f"Stop Loss:          ${stopLoss}\n"
+    for i in range(0, len(targets)):
+        report += f"Target {i + 1}: ${targets[i]}\n"
+    for i in range(0, len(targetShares)):
+        report += f"Target {i + 1} split: {targetShares[i]}"
+        if i != len(targetShares) - 1: report += '\n'
     return report
 
 def generate_position(positionData: dict) -> str:
@@ -170,8 +119,8 @@ def generate_position(positionData: dict) -> str:
     if not check_if_full(setupTable):
         return "ERROR: Please fill out your setup properties"
     elif setupTable["direction"] == "long":
-        return long_position(setupTable)
+        return long_position(setupTable, conf.DEFAULT_CONFIG)
     elif setupTable["direction"] == "short":
-        return short_position(setupTable)
+        return short_position(setupTable, conf.DEFAULT_CONFIG)
     else:
         return "ERROR: No direction specified, please set direction and try again"
